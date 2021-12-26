7ceed5f917e5040199234e4f8cf976d5efd5e59c
Adam Callanan <a.callanan@sbcglobal.net>
2021-12-26 14:53:45 -0600

    refactor

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from tasks.game_initializer import initialize_game
 from tasks.mansion_builder import setup_mansion
 import datetime
 
-start_time = datetime.datetime.now()
-print("~ MYSTERY MANSION STARTING @ " + str(start_time))
-print()
+def log_start_message(start_time):
+  print("~ MYSTERY MANSION STARTING @ " + str(start_time))
+  print()
 
+def log_end_message(start_time, end_time):
+  length = end_time - start_time
+  print()
+  print("~ MYSTERY MANSION ENDING   @ " + str(end_time))
+  print("~ Script ran in " + str(length))
+
+# start
+start_time = datetime.datetime.now()
+log_start_message(start_time)
+
+# run
 id = initialize_game()
 assets = setup_clues(id)
 setup_mansion(id, assets)
 
+# end
 end_time = datetime.datetime.now()
-print()
-print("~ MYSTERY MANSION ENDING   @ " + str(end_time))
-print("~ Script ran in " + str(end_time - start_time))
+log_end_message(start_time, end_time)
