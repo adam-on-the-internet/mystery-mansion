7a85ba511c4722205d3b2fd72f1cdcdf5c84a84f
Adam Callanan <a.callanan@sbcglobal.net>
2022-01-03 17:26:35 -0600

    check if mansion is valid before accepting

--- tasks/game_generator.py
+++ tasks/game_generator.py
@@ -1,9 +1,17 @@
 from tasks.mansion_generator import generate_mansion
-from tasks.mansion_tester import test_mansion
+from tasks.mansion_tester import is_mansion_valid
 from tasks.mansion_saver import save_mansion
 
 def generate_game(style):
-  mansion = generate_mansion(style.lower())
-  test_mansion(mansion)
+  waiting_for_valid_game = True
+  mansion = None
+  
+  while waiting_for_valid_game:
+    mansion = generate_mansion(style.lower())
+    is_valid = is_mansion_valid(mansion)
+    if is_valid:
+      # stop & submit when we get a valid mansion, else we keep trying to generate one
+      waiting_for_valid_game = False
+
   save_mansion(mansion)
   return mansion
--- tasks/mansion_tester.py
+++ tasks/mansion_tester.py
@@ -1,22 +1,25 @@
 from tasks.style_reader import read_rooms_setting, read_locked_rooms, read_furniture_setting, read_clues_setting
 
-def test_id(id):
+def test_id(errs, id):
   if id is None:
-    print("ERR: ID must be set.")
+    errs.append("ID must be set.")
   elif len(id) != 4:
-    print("ERR: ID must be 4 characters.")
+    errs.append("ID must be 4 characters.")
+  return errs
 
-def test_spaces(style, spaces):
+def test_spaces(errs, style, spaces):
   expected = read_rooms_setting(style)
   if expected != len(spaces):
-    print("ERR: Incorrect number of rooms generated.")
+    errs.append("Incorrect number of rooms generated.")
+  return errs
 
-def test_locked_spaces(style, locked_spaces):
+def test_locked_spaces(errs, style, locked_spaces):
   expected = read_locked_rooms(style)
   if expected != len(locked_spaces):
-    print("ERR: Incorrect number of locked rooms generated.")
+    errs.append("Incorrect number of locked rooms generated.")
+  return errs
 
-def test_unlocked_spaces(style, unlocked_spaces):
+def test_unlocked_spaces(errs, style, unlocked_spaces):
   total_clues = read_clues_setting(style)
   clues_per_space = 2 # TODO make clues-per-space set in style/settings
   unlocked_clue_interactions = 0
@@ -26,24 +29,38 @@ def test_unlocked_spaces(style, unlocked_spaces):
         unlocked_clue_interactions = unlocked_clue_interactions + 1
   unlocked_clues = unlocked_clue_interactions * clues_per_space
   if total_clues > unlocked_clues:
-    print("ERR: The clues are set incorrectly, the game may become unwinnable.")
+    errs.append("The clues are set incorrectly, the game may become unwinnable.")
+  return errs
 
-def test_furniture(style, interactions):
+def test_furniture(errs, style, interactions):
   expected = read_furniture_setting(style)
   if expected != len(interactions):
-    print("ERR: Incorrect number of furniture generated.")
+    errs.append("Incorrect number of furniture generated.")
+  return errs
 
-def test_furniture_per_room(spaces):
+def test_furniture_per_room(errs, spaces):
   for space in spaces:
     if len(space.interactions) < 2:
-      print("ERR: A room was generated incorrectly, " + space.room.name + " has less than 2 items of furniture.")
+      errs.append("A room was generated incorrectly, " + space.room.name + " has less than 2 items of furniture.")
     elif len(space.interactions) > 5:
-      print("ERR: A room was generated incorrectly, " + space.room.name + " has more than 5 items of furniture.")
+      errs.append("A room was generated incorrectly, " + space.room.name + " has more than 5 items of furniture.")
+  return errs
 
-def test_mansion(mansion):
-  test_id(mansion.id)
-  test_spaces(mansion.style, mansion.spaces)
-  test_locked_spaces(mansion.style, mansion.get_locked_spaces())
-  test_unlocked_spaces(mansion.style, mansion.get_unlocked_spaces())
-  test_furniture(mansion.style, mansion.get_all_interactions())
-  test_furniture_per_room(mansion.spaces)
+def is_mansion_valid(mansion):
+  errs = []
+  errs = test_id(errs, mansion.id)
+  errs = test_spaces(errs, mansion.style, mansion.spaces)
+  errs = test_locked_spaces(errs, mansion.style, mansion.get_locked_spaces())
+  errs = test_unlocked_spaces(errs, mansion.style, mansion.get_unlocked_spaces())
+  errs = test_furniture(errs, mansion.style, mansion.get_all_interactions())
+  errs = test_furniture_per_room(errs, mansion.spaces)
+  if len(errs) > 0:
+    print()
+    print("Errors found with mansion " + mansion.id + ":")
+    for err in errs:
+      print(err)
+    print()
+    return False
+  else:
+    return True
+  # TODO more tests? better test setup?
